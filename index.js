08e9460045e0d5f70fc662842f452ce270528fce
Benn <bennkiche7@gmail.com>
2024-09-29 14:23:30 +0300

    firstcommit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+  }
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet <= 400) {
+      return 0; // Free for the first 400 feet
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02; // 2 cents per foot after 400 feet
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25; // Flat fee
+    } else {
+      return 'cannot travel that far'; // Over 2500 feet
+    }
+  }
